832eb28749d36a221fda703c7c6e8e095469f9d2
Rinzin Tenzin <tenzin.rire@gmail.com>
2023-03-14 23:04:40 +0100

    dummy data in DB & sql queries in .txt

---
+++ src/includes/Db.class.php
@@ -0,0 +1,37 @@
+<?php
+
+class Db
+{
+
+    // Database configuration
+    private $host = 'ID396978_phpFuncProgram.db.webhosting.be';
+    private $dbname = 'ID396978_phpFuncProgram';
+    private $username = 'ID396978_phpFuncProgram';
+    private $password = 'rootroot!123';
+    private $port = 3306;
+    private $pdo;
+
+    // Create a new PDO instance
+    public function __construct()
+    {
+        if ($this->pdo === null) {
+            try {
+                $this->pdo = new PDO(
+                    "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->dbname . ";charset=utf8mb4",
+                    $this->username,
+                    $this->password
+                );
+            } catch (PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+                return null;
+            }
+        }
+    }
+
+    public function executeQuery($sql, $filters = [], $fetch = PDO::FETCH_OBJ)
+    {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($filters);
+        return $stmt->fetchAll($fetch);
+    }
+}
