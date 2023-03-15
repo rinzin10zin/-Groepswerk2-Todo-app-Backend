306d7ddeca923d7cff16227d3fe2307a419acdf1
Niels Thuis <cnops_niels@hotmail.com>
2023-03-15 17:55:02 +0100

    Ticket12
    Made basic getAll path if you have no args, made a path for the listsOverview at localhost/api/lists

---
+++ src/api.php
@@ -0,0 +1,36 @@
+<?php
+require "./includes/Db.class.php";
+require "./includes/Lists.class.php";
+
+// $args['qsparts'] = explode('/', $args['qs']);
+
+
+
+$args = $_REQUEST; //Gets the Query from the url
+// var_dump($args); //If you check this for http://localhost/api/lists/2, you get array{resource=>lists, id=>2}
+$method = $_SERVER["REQUEST_METHOD"]; //Gets the method from the requests; eg. GET, POST, DELETE
+$response = new StdClass;
+$response->args = $args; //Easy for developing, so you can see the query in the response
+
+//Open Database connection
+$db = new Db();
+$Lists = new Lists($db);
+
+if ($args == []) {
+    $response->data = $Lists->getAll();
+    header('Content-Type: application/json; charset=utf-8');
+    print json_encode($response);
+    exit;
+}
+
+
+if ($args["resource"] == "lists") {
+    $response->data = $Lists->getAllLists();
+    header('Content-Type: application/json; charset=utf-8');
+    print json_encode($response);
+    exit;
+}
+
+header('Content-Type: application/json; charset=utf-8');
+print json_encode($response);
+exit;
