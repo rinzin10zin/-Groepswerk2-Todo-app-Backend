af951cae016dc21528e706cd132cacb8f7b842f6
Niels Thuis <cnops_niels@hotmail.com>
2023-03-20 23:25:15 +0100

    Ticket 48
    Removed $_SERVER["document root"]

--- src/includes/Db.class.php
+++ src/includes/Db.class.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require $_SERVER["DOCUMENT_ROOT"] . "/todos/includes/options.env.php";
+require "./includes/options.env.php";
 
 class Db
 {
